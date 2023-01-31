91bc753570fa99363ce2f2f883aa33948982d207
Dan Monolith <monolithicmail@gmail.com>
2023-01-31 18:17:29 +0500

    Implement hover and create resize button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+// Create the container that will hold the pixels.
+// Iterate over an index to get the right grid size.
+// Add class and index to each created div and append to container.
 const square = document.querySelector('.square');
 const divNumber = 256;
 for (let i = 0; i < 256; i++) {
@@ -9,11 +11,28 @@ for (let i = 0; i < 256; i++) {
     document.getElementById("square").appendChild(div);    
 }
 
+//const pixels = document.querySelectorAll(".pixel");
+//pixels.forEach((div) => {
+//    div.addEventListener("mouseover", () => {
+//        console.log(div.id);
+//    });
+//});
 
+// Select all pixels and create a nodeList to loop through to attach eventListeners to.
 const pixels = document.querySelectorAll(".pixel");
 pixels.forEach((div) => {
-    div.addEventListener('click', () => {
-        console.log(div.id);
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
     });
 });
+
+// Add button that sends popup asking for the no. of squares per side. Limit 100.
+// New grid must generate in the same total space as before. 
+const button = document.getElementById(".resize");
+button.onclick = function (event) {
+    alert("Testing...");
+}
+
+
+
 
