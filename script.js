91b0b8a07adc7a4ba364be1022c20c35eca9140f
Dan Monolith <monolithicmail@gmail.com>
2023-03-08 10:50:57 +0500

    Implement CSS Grid through JS. Get size from slider, use it to draw grid. Auto clear grid

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-const defaultSize = 16;
-
-let currentSize = defaultSize;
-
-function setCurrentSize(newSize) {
-    currentSize = newSize;
-}
-
-const sizeValue = document.getElementById('sizeValue');
+//global consts.
+const canvass2 = document.getElementById('canvass2');
 const sizeSlider = document.getElementById('sizeSlider');
-const grid = document.getElementById('grid');
+let size = 16;
 
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
-
-function changeSize(value) {
-    setCurrentSize(value);
-    updateSizeValue(value);
-    reloadGrid();
-};
 
 function updateSizeValue(value) {
     sizeValue.innerHTML = `${value} x ${value}`;    
 };
 
-function reloadGrid() {
+function changeSize(value) {
+    size = value;
     clearGrid();
-    setupGrid(currentSize);
+    drawGrid(size);
+}
+// Create the container that will hold the pixels.
+// Iterate over an index to get the right grid size.
+// Add class and index to each created div and append to container.
+const canvass = document.querySelector('.canvass');
+let divNumber = 256;
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+    div.id = i + 1;
+    div.textContent = " ";
+    document.getElementById("canvass").appendChild(div);    
 }
 
-function clearGrid() {
-    grid.innerHTML = '';
-};
+// Select all pixels and create a nodeList to loop through to attach eventListeners.
+const pixels = document.querySelectorAll(".pixel");
+pixels.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    });
+});
 
+const resetButton = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+    pixels.forEach((div) => {
+        div.style.backgroundColor = "white";
+    });
+});
 
-
-function setupGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+function drawGrid(size) {
+    const canvass2 = document.querySelector('.canvass2');
     for (let i = 0; i < size * size; i++) {
-        const gridElement = document.createElement('div');
-        gridElement.classList.add('grid-element');
-        //gridElement.addEventListener('mouseover', changeColor);
-        //gridElement.addEventListener('mousedown', changeColor);
-        grid.appendChild(gridElement); 
-    };
+        const div = document.createElement('div');
+        div.classList.add('pixel2');
+        div.id = i + 1; 
+        document.getElementById("canvass2").appendChild(div); 
+    }
+    
+    canvass2.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvass2.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 };
 
 window.onload = () => {
-    setupGrid(defaultSize);
+    drawGrid(size);
 };
 
-
+function clearGrid() {
+    canvass2.textContent = '';
+};
