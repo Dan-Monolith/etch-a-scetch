b05ff805b2ab07f45cbcd614c759e09158daa54a
Dan Monolith <monolithicmail@gmail.com>
2023-03-13 13:35:42 +0500

    Implement click and drag. Start on fade and rainbow mode.

--- script.js
+++ script.js
@@ -30,21 +30,45 @@ function drawGrid(size) {
     canvass.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canvass.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    // Select all pixels and create a nodeList to loop through to attach eventListeners.
+    switchGrid();
+    switchReset();
+
+    let mouseIsDown = false;
+    document.addEventListener('mousedown', () => {
+        mouseIsDown = true;
+    });
+    document.addEventListener('mouseup', () => {
+        mouseIsDown = false;
+    });
+
     const pixels = document.querySelectorAll('.pixel');
     pixels.forEach((div) => {
         div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'black';
+            if(mouseIsDown === true){
+                div.classList.add(`${drawingMode}`);
+            };    
         });
     });
+    
+    pixels.forEach((div) => {
+        div.addEventListener('click', () => {
+            div.classList.add(`${drawingMode}`);
+        });
+    });
+};
 
+function switchReset (){
+    const pixels = document.querySelectorAll('.pixel');
     const resetButton = document.querySelector('.resetBtn');
     resetBtn.addEventListener('click', () => {
         pixels.forEach((div) => {
-            div.style.backgroundColor = 'white';
+            div.classList.remove(`${drawingMode}`);
         });
     });
+};
 
+function switchGrid (){
+    const pixels = document.querySelectorAll('.pixel');
     const gridButton = document.querySelector('.gridBtn');
     gridBtn.addEventListener('click', () => {
         pixels.forEach((div) => {
@@ -52,7 +76,17 @@ function drawGrid(size) {
         });
     });
 };
-//instead of just div.style.border = none, use toggle to activate/deactivate
+
+let drawingMode = 'ink';
+
+function switchFade (){
+    const fadeButton = document.querySelector('.fadeBtn');
+    fadeBtn.addEventListener('click', () => {
+        let drawingMode = 'fade';
+        console.log(drawingMode);
+    })
+    
+};
 
 window.onload = () => {
     drawGrid(size);
